ba229e710bc7ea7458a8c845d5344342e11cdcb6
バング ドマス <bangdompret@gmail.com>
2021-04-07 12:47:49 +0700

    *

---
+++ api.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+logo = '''
+
+ .--. .-----. .--. .-..-..-..-..---.  .--.  .--. .-..-.
+: ,. :`-. .-': .; :: :' ;: :: :: .  :: .--': .--': :: :
+: :: :  : :  :    ::   ' : :: :: :: :: `;  `. `. : :: :
+: :; :  : :  : :: :: :.`.: :; :: :; :: :__  _`, :: :; :
+`.__.'  :_;  :_;:_;:_;:_;`.__.':___.'`.__.'`.__.'`.__.'
+                                                       
+fb.me/bangdomath.id
+'''
+print(logo)
+
+nama = raw_input("Anime: ")
+response = requests.get("https://anime.kaedenoki.net/api/anime/" + nama)
+data = response.json()
+print(f'Title: {data["japanase"]}')
+print(f'Score: {data["score"]}')
+print(f'Status: {data["status"]}')
+print(f'Total Eps: {data["total_episode"]}')
+print(f'Sinopsis: {data["synopsis"]}')
+print(f'Link: {data["batch_link"], ["link"]}')
